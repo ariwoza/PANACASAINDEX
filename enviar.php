cd30b7d0005564210c7242a4e597b0b4d46f2b9f
ariwoza <58341730+ariwoza@users.noreply.github.com>
2020-02-25 12:42:47 -0500

    sss

---
+++ enviar.php
@@ -0,0 +1,35 @@
+<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "ariwoza@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje-de-envio.html');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>Mensaje enviado</h1> 
+</body>
+</html>
--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,10 @@
-
-    jQuery(document).ready(function() {
-  jQuery(window).scroll(function() {
-    if (jQuery(this).scrollTop() > 1000) {
-      jQuery('.logo').addClass('logoscroll');
-    } else{
-         jQuery('.logo').removeClass('logoscroll');
+window.onscroll = function() {
+    if (document.body.scrollTop > 90|| document.documentElement.scrollTop > 90) {
+        console.log('si funciona');
+        document.getElementById("header").classList.add("headerbg")
     }
-  });
-});
-
+    else {
+        console.log('nop');
+        document.getElementById("header").classList.remove("headerbg")
+    }
+}
